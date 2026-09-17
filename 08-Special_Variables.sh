89f4fd448953084d3b241644fb95ff7b1aa67118
Harish Malvi <harish.malvi@gmail.com>
2026-09-17 15:19:08 -0600

    some message

---
+++ 08-Special_Variables.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "All Variables are: $@" # Here we are using the special variable $@ to display all the arguments passed to the script.
+
+echo "All Variables are: $*" # Here we are using the special variable $* to display all the arguments passed to the script.
