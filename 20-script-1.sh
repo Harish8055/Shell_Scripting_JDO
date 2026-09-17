7248964e5375fd239a4370ae5490f91598c82942
Harish Malvi <harish.malvi@gmail.com>
2026-09-17 17:20:45 -0600

    some message

---
+++ 20-script-1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+NAME=INDIA
+
+echo "My Country: $NAME"
+echo  "PID of the current script is: $$"
+source ./21-script-2.sh
---
+++ 21-script-2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "My Country: $NAME"
+echo  "PID of the script-2 is: $$"
