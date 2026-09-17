f81be9a53bbe66a5aa90144d72726064b6cfea13
Harish Malvi <harish.malvi@gmail.com>
2026-09-17 14:52:28 -0600

    some message

---
+++ 05-Variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "plese enter the pin number"
+read -s PIN_NUMBER # Here PIN_NUMBER is a variable that will store the user input. The -s option is used to hide the input while typing.    
+
+echo "PIN Entered is $PIN_NUMBER" # Here we are using the variable PIN_NUMBER to display the entered pin number.
+
