74680d7128fc4701eb589ab6ddd71634571bae30
Harish Malvi <harish.malvi@gmail.com>
2026-09-17 17:01:47 -0600

    some message

---
+++ 14-colors.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+    R="\e[38m"
+    G="\e[32m"
+    B="\e[34m"
+    Y="\e[33m"
+    P="\e[35m"
+    C="\e[36m"
+    W="\e[37m"
+
+    echo -e "${R}This is Red Color"
+    echo -e "${G}This is Green Color"
+    echo -e "${B}This is Blue Color"
+    echo -e "${Y}This is Yellow Color"              
+    
+
