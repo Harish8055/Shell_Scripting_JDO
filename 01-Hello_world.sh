73486b1117fa794a8f470a2a8f5f0a47f761505a
Harish Malvi <harish.malvi@gmail.com>
2026-09-17 14:16:11 -0600

    updated

--- 01-Hello_world.sh
+++ 01-Hello_world.sh
@@ -2,3 +2,4 @@
 
 # This is a simple bash script that prints "Hello, World!" to the terminal.
 echo "Hello, World!"
+echo "This is a simple bash script that demonstrates how to print text to the terminal."
