f1a68c6227350b0e5d31ed68690093f5e2980dfb
Harish Malvi <harish.malvi@gmail.com>
2026-09-17 15:23:52 -0600

    some message

--- 08-Special_Variables.sh
+++ 08-Special_Variables.sh
@@ -3,3 +3,11 @@
 echo "All Variables are: $@" # Here we are using the special variable $@ to display all the arguments passed to the script.
 
 echo "All Variables are: $*" # Here we are using the special variable $* to display all the arguments passed to the script.
+
+echo  "Script Name is: $0" # Here we are using the special variable $0 to display the name of the script.
+
+echo "current process Directory: $PWD" # Here we are using the special variable $$ to display the current process ID of the script.
+
+echo "Who  is running the script: $USER" # Here we are using the special variable $USER to display the username of the user running the script.
+echo "home directory of the user: $HOME" # Here we are using the special variable $HOME to display the home directory of the user running the script.
+
