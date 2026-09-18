0d74c44c51ca81cba3d86f17932a2ad6101f0c9f
Harish Malvi <harish.malvi@gmail.com>
2026-09-17 21:46:25 -0600

    some message

--- 23-while-loop.sh
+++ 23-while-loop.sh
@@ -1,11 +1,16 @@
-#!/bin/bash
+# #!/bin/bash
 
-count=5
+# count=5
 
-echo "stargin coundown..."
-while [ $count -gt 0 ]
-do
-echo "time left: $count"
-sleep 1
-count=$((count - 1))
-done    
+# echo "stargin coundown..."
+# while [ $count -gt 0 ]
+# do
+# echo "time left: $count"
+# sleep 1
+# count=$((count - 1))
+# done    
+
+while IFS=read -r line; do
+    echo "processing line: $line"
+
+done < "20-script-1.sh"
